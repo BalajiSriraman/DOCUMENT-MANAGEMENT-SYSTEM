e4515c78777460eff7ec7ee7403e2624e59f74b7
2005balaji <balajisr000@gmail.com>
2023-07-09 16:43:29 +0530

    My commit message

--- client/src/components/Upload.tsx
+++ client/src/components/Upload.tsx
@@ -1,78 +1,70 @@
-import { useDropzone } from "react-dropzone";
+import React, { useState } from "react";
+import axios from "axios";
 
-function Upload() {
+const UploadFileForm: React.FC = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [fileName, setFileName] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-    const { getRootProps, getInputProps } = useDropzone({
-        onDrop: async (acceptedFiles) => {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files && event.target.files[0];
+    setFile(selectedFile);
+  };
 
-            const data = {
-                name: "hello from client",
-                // file: acceptedFiles[0].name
-            }
-            try {
-                // console.log(data.file)
-                const response = await fetch("http://localhost:1403/files", {
-                    method: "POST",
-                    body: data,
-                });
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFileName(event.target.value);
+  };
 
-                if (response.ok) {
-                    console.log("Files uploaded successfully!");
-                    console.log(response)
-                    // Handle success
-                } else {
-                    console.error("Failed to upload files");
-                    console.log(response)
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
 
-                    // Handle error
-                }
-            } catch (error) {
-                console.error("Error occurred while uploading files", error);
-                // Handle error
-            }
-        }
-        ,
-        accept: {
-            'pdf/file': ['.pdf']
-        }
-    });
+    if (!file || fileName === "") {
+      setErrorMessage("Please select a file and enter a folder name");
+      return;
+    }
 
-    return (
-        <div className="flex items-center justify-center w-full mt-20">
-            <div className="flex items-center justify-center w-4/5" {...getRootProps()}>
-                <label
-                    htmlFor="dropzone-file"
-                    className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                >
-                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg
-                            aria-hidden="true"
-                            className="w-10 h-10 mb-3 text-gray-400"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                            ></path>
-                        </svg>
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                            <span className="font-semibold">Click to upload</span> or drag and
-                            drop
-                        </p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">
-                            SVG, PNG, JPG or GIF (MAX. 800x400px)
-                        </p>
-                    </div>
-                    <div id="dropzone-file" className="hidden" {...getInputProps()} />
-                </label>
-            </div>
-        </div>
-    );
-}
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("folderName", fileName);
 
-export default Upload;
+    try {
+      await axios.post("http://localhost:1403/files", formData);
+      console.log("File uploaded successfully");
+    } catch (error) {
+      console.log("420 error occurred while uploading the file:", error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Upload file
+        </label>
+        <input
+          className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          id="multiple_files"
+          type="file"
+          accept=".pdf"
+          onChange={handleFileChange}
+          required // added required attribute
+        />
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Select folder
+        </label>
+        <input
+          type="text"
+          placeholder="Enter or choose folder name"
+          value={fileName}
+          onChange={handleNameChange}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required // added required attribute
+        />
+        <button type="submit" >Upload</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default UploadFileForm;
