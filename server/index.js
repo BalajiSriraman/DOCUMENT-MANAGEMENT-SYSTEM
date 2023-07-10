ba4b4952bc60f71cd29a825a2c612ac4eb7bbf11
2005balaji <balajisr000@gmail.com>
2023-07-10 16:41:43 +0530

    My commit message

--- server/index.js
+++ server/index.js
@@ -1,45 +1,8 @@
-// const cors = require('cors');
-// const express = require('express');
-// const multer = require('multer');
-// const path = require('path');
-
-// const app = express();
-// app.use(cors());
-
-// const storage = multer.diskStorage({
-//   destination: './uploads',
-//   filename: (req, file, callback) => {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//     const fileExtension = path.extname(file.originalname);
-//     callback(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-
-// // app.post('/test',(req,res)=>{}))
-
-// app.post('/files', upload.single('file'), (req, res) => {
-//   console.log('Folder Name:', req.body.folderName);
-  
-//   if (!req.file) {
-//     console.log('No file uploaded');
-//   } else {
-//     console.log('File uploaded:', req.file.originalname);
-//   }
-  
-//   return res.status(200).json({ message: 'Request received' });
-// });
-
-// app.listen(1403, () => {
-//   console.log('Server is running on port 1403');
-// });
-
-
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
-const app = express(cors());
+const fs = require('fs');
+const app = express();
 const port = 1403;
 
 // Multer configuration for file upload
@@ -58,7 +21,20 @@ app.post('/files', upload.single('file'), (req, res) => {
   console.log('File exists');
   console.log('Folder:', folder);
 
-  res.status(200).send('File exists');
+  // Create the folder if it doesn't exist
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
+
+  // Save the file inside the folder
+  fs.writeFile(`${folder}/${file.originalname}`, file.buffer, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error saving the file');
+    }
+    console.log('File saved successfully');
+    res.status(200).send('File saved successfully');
+  });
 });
 
 // Start the server
--- server/index.ts
+++ server/index.ts
@@ -1,77 +1,52 @@
-import cors from 'cors';
 import express, { Request, Response } from 'express';
-import multer, { diskStorage } from 'multer';
-import path from 'path';
+import multer from 'multer';
+import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
+const port = 1403;
+
+// Multer configuration for file upload
+const upload = multer();
+
+// Enable CORS
 app.use(cors());
 
-const storage = diskStorage({
-  destination: `./uploads`,
-  filename: (req, file, callback) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const fileExtension = path.extname(file.originalname);
-    callback(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
+// Function to save the file
+function saveFile(folder: string, file: Express.Multer.File, res: Response) {
+  // Create the folder if it doesn't exist
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
   }
-});
 
-const upload = multer({ storage: storage });
+  // Save the file inside the folder
+  fs.writeFile(`${folder}/${file.originalname}`, file.buffer, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error saving the file');
+    }
+    console.log('File saved successfully');
+    res.status(200).send('File saved successfully');
+  });
+}
 
+// POST route for file upload
 app.post('/files', upload.single('file'), (req: Request, res: Response) => {
-  console.log(req.body.folderName);
   const file = req.file;
+  const folder = req.body.folderName;
+
   if (!file) {
-    return res.status(430).json({ error: 'No file uploaded', data: req.body });
+    console.log('No file selected');
+    return res.status(400).send('No file selected');
   }
 
-  // Process the file or save it to the database
+  console.log('File exists');
+  console.log('Folder:', folder);
 
-  return res.status(200).json({ message: 'File uploaded successfully', data: req.body });
+  saveFile(folder, file, res);
 });
 
-app.listen(1403, () => {
-  console.log('Server is running on port 1403');
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-
-// import cors from 'cors';
-// import express, { Request, Response } from 'express';
-// import fs from 'fs';
-// import path from 'path';
-
-// const app = express();
-// app.use(cors());
-
-// const uploadDirectory = './uploads'; // Specify the directory where you want to save the files
-
-// app.post('/files', (req:any, res:any) => {
-//   const file = req.body.file; // Update this line to use the correct property based on how the file is sent in the request
-
-//   if (!file) {
-//     console.log('file not exists');
-//     return res.status(430).json({ error: 'No file uploaded', data: req.body });
-//   }else{
-//     console.log('file exists');
-//   }
-
-//   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//   const fileExtension = path.extname(file.originalname);
-//   const fileName = file.fieldname + '-' + uniqueSuffix + fileExtension;
-//   const filePath = path.join(uploadDirectory, fileName);
-
-//   // Write the file to the specified directory using fs.writeFile
-//   fs.writeFile(filePath, file.data, (err) => {
-//     if (err) {
-//       console.error(err);
-//       return res.status(500).json({ error: 'Failed to save the file', data: req.body });
-//     }
-
-//     // Process the file or save it to the database
-
-//     return res.status(200).json({ message: 'File uploaded successfully', data: req.body });
-//   });
-// });
-
-// app.listen(1403, () => {
-//   console.log('Server is running on port 1403');
-// });
