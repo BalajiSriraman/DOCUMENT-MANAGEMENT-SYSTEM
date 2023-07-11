94f07446e2f1d8aa64ffd25a1805f64f8fba01c8
2005balaji <balajisr000@gmail.com>
2023-07-11 11:52:41 +0530

    multer

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,19 @@
 import Fastify, { FastifyInstance } from 'fastify';
-import cors from '@fastify/cors'
+import express from 'express'
+// import cors from '@fastify/cors'
+import cors from 'cors'
 import formidable from 'formidable'
 import fs from 'fs'
 import path from 'path'
+import { Request } from "express";
+const app = express()
+app.use(cors<Request>());
 
-export const fastify: FastifyInstance = Fastify({})
- fastify.register(cors, {
-    // put your options here
-})
+export const fastify: FastifyInstance = Fastify({})     
+
+// fastify.register(cors, {
+//     // put your options here
+// })
 
 fastify.get('/', async (request, reply) => {
 
@@ -18,33 +24,36 @@ fastify.get('/', async (request, reply) => {
 })
 
 // Endpoint to handle file uploads
-fastify.post('/files', (request, reply) => {
-    console.log('request')
+app.post('/files', (request, reply) => {
     const form = formidable({ multiples: true });
-  
-    // Parse the incoming form data
-    form.parse(request.raw, (err, fields, files) => {
-      if (err) {
-        console.error('Error parsing form data', err);
-        reply.code(500).send({ error: 'Failed to process the uploaded file(s).' });
-        return;
-      }
-  
-      // Move the uploaded file(s) to the desired folder
-      const folder = fields.folder || 'default';
-      const uploadDir = path.join(__dirname, 'uploads', 'folder');
-  
-      fs.mkdirSync(uploadDir, { recursive: true });
-  
-      // Move each file to the upload directory
-    //   Object.values(files).forEach((file) => {
-    //     const oldPath = file.path;
-    //     const newPath = path.join(uploadDir, file.name);
-  
-    //     fs.renameSync(oldPath, newPath);
-    //   });
-  
-      reply.send({ message: 'File(s) uploaded successfully.' });
-    });
-  });
-  
+    console.log(form)
+
+    // // Parse the incoming form data
+    // form.parse(request.raw, (err, fields, files) => {
+    //   if (err) {
+    //     console.error('Error parsing form data', err);
+    //     reply.code(500).send({ error: 'Failed to process the uploaded file(s).' });
+    //     return;
+    //   }
+
+    //   // Move the uploaded file(s) to the desired folder
+    //   const folder = fields.folder || 'default';
+    //   const uploadDir = path.join(__dirname, 'uploads', 'folder');
+
+    //   fs.mkdirSync(uploadDir, { recursive: true });
+
+    //   // Move each file to the upload directory
+    // //   Object.values(files).forEach((file) => {
+    // //     const oldPath = file.path;
+    // //     const newPath = path.join(uploadDir, file.name);
+
+    // //     fs.renameSync(oldPath, newPath);
+    // //   });
+
+    reply.send({ message: 'File(s) uploaded successfully.' });
+    // });
+});
+
+app.listen(1403, () => {
+    console.log('Server listening on http://localhost:1403 ...');
+})
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,34 @@
-import { fastify } from "./app";
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import multer, { diskStorage } from 'multer';
+import path from 'path';
 
-fastify.listen({ port: 1403 }, (err, address) => {
-  console.error(`server listening  on specified port`);
+const app = express();
+app.use(cors());
+
+const storage = diskStorage({
+  destination: 'uploads/',
+  filename: (req, file, callback) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const fileExtension = path.extname(file.originalname);
+    callback(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
+  }
 });
-  
+
+const upload = multer({ storage: storage });
+
+app.post('/files', upload.single('file'), (req: Request, res: Response) => {
+  console.log('req init');
+  const file = req.file;
+  if (!file) {
+    return res.status(430).json({ error: 'No file uploaded', data: req.body });
+  }
+
+  // Process the file or save it to the database
+
+  return res.status(200).json({ message: 'File uploaded successfully', data: req.body });
+});
+
+app.listen(1403, () => {
+  console.log('Server is running on port 1403');
+});
